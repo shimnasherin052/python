78ea2f51893bb877de71a4c61fa70284706ee745
shimnasherin052 <shimnasherin052@gmail.com>
2024-12-18 13:51:12 +0530

    Add files via upload

---
+++ swap_without_temp.py
@@ -0,0 +1,9 @@
+a=int(input("enter the number"))
+b=int(input("enter the number"))
+print("Before swapping")
+print("a= ",a,"b=",b)
+a=a+b
+b=a-b
+a=a-b
+print("after swapping")
+print("b= ",a,"b=",b)
