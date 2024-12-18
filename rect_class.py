db7a22e14537c732c6c1a8c2d80343afa1ad8135
shimnasherin052 <shimnasherin052@gmail.com>
2024-12-18 14:02:12 +0530

    Add files via upload

---
+++ rect_class.py
@@ -0,0 +1,26 @@
+class Rectangle:
+    def __init__(self, l, w):
+        self.length = l
+        self.breadth = w
+
+    def rectangle_area(self):
+        return self.length * self.breadth
+
+    def rectangle_peri(self):
+        return 2 * (self.length + self.breadth)
+
+# Creating two Rectangle instances
+rectangle1 = Rectangle(12, 10)
+rectangle2 = Rectangle(6, 5)
+
+# Printing the areas and perimeters
+print("Area1 =", rectangle1.rectangle_area())
+print("Area2 =", rectangle2.rectangle_area())
+print("Perimeter1 =", rectangle1.rectangle_peri())
+print("Perimeter2 =", rectangle2.rectangle_peri())
+
+# Checking if the areas are equal
+if rectangle1.rectangle_area() == rectangle2.rectangle_area():
+    print("Areas are Equal")
+else:
+    print("Areas are not Equal")
