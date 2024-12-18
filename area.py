6c14b4b387a46091254599240ee7f7a8979a945f
shimnasherin052 <shimnasherin052@gmail.com>
2024-12-18 13:54:05 +0530

    Add files via upload

---
+++ area.py
@@ -0,0 +1,12 @@
+r=int(input("enter the cirlce"))
+R=3.14*r*r
+print("area of a rectangle")
+b=int(input("enter the length"))
+l=int(input("enter the length"))
+r=l*b
+print(R)
+print("area of a triangle")
+h=int(input("enter the height"))
+b=int(input("enter the base"))
+T=0.5*b*h
+print(T)
